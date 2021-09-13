673c41decfafbb97eeda3f4b42661ef3f8484669
Your Name <you@example.com>
2021-09-14 00:26:13 +0530

    Added: Players order

--- Players.java
+++ Players.java
@@ -1,10 +1,31 @@
 package cardgames;
 
+import java.util.*;
+
+//player class
 public class Players {
 	int noOfPlayers;
-	Players(int n){
-		System.out.println(n+ " players get ready to play");
-		noOfPlayers=n;
+	Random rand = new Random();
+
+	Players(int n) {
+		System.out.println(n + " players get ready to play");
+		noOfPlayers = n;
 	}
-	
+
+	public void PlayersOrder() {
+		int order;
+		int count = 0;
+		ArrayList<Integer> sequence = new ArrayList<Integer>(noOfPlayers);
+		
+		while (noOfPlayers > count) {
+			order = rand.nextInt(4);
+			if (sequence.contains(order)) {
+				continue;
+			} else {
+				sequence.add(order);
+				count++;
+			}
+		}
+	}
+
 }
